c55a1d053eab72fcff2e013cbe8a26dfc1f88b7d
Håvard G <nirentos@gmail.com>
2024-05-02 21:16:06 +0200

    updated project for java

---
+++ demo/src/test/java/com/data1700_oblig/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data1700_oblig.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
